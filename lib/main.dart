43a98e783225fbbd45faa218b64a7cf20e7f6f22
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-07-07 22:18:12 +0300

    Made the container changes randomly.

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,67 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyAnimatedContainer());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyAnimatedContainer extends StatefulWidget {
+  @override
+  _MyAnimatedContainerState createState() => _MyAnimatedContainerState();
+}
+
+class _MyAnimatedContainerState extends State<MyAnimatedContainer> {
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.blue;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(50);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: MyHomePage(),
-    );
-  }
-}
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              final random = Random();
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[],
-          )
-        ],
+              //change the width and the height randomly
+              _width = random.nextInt(300).toDouble();
+              _height = random.nextInt(300).toDouble();
+
+              //change the color randomly
+              _color = Color.fromRGBO(
+                random.nextInt(256),
+                random.nextInt(256),
+                random.nextInt(256),
+                1,
+              );
+
+              //change the border radius randomly.
+              _borderRadius =
+                  BorderRadius.circular(random.nextInt(100).toDouble());
+            });
+          },
+          child: Icon(Icons.play_arrow),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                AnimatedContainer(
+                  duration: Duration(seconds: 1),
+                  width: _width,
+                  height: _height,
+                  decoration:
+                      BoxDecoration(color: _color, borderRadius: _borderRadius),
+                  curve: Curves.fastOutSlowIn,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
