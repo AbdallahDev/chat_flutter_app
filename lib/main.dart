abfb18e124333bcdebb128e15ca5ffb58a4220b7
abdallah.sarayrah <abdallah.dev85@gmail.com>
2019-07-08 23:32:52 +0300

    Revert "I've rested the branch"
    This reverts commit e3e2d5eb

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,31 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyAnimatedContainer());
+void main() => runApp(MyApp());
 
-class MyAnimatedContainer extends StatefulWidget {
-  @override
-  _MyAnimatedContainerState createState() => _MyAnimatedContainerState();
-}
-
-class _MyAnimatedContainerState extends State<MyAnimatedContainer> {
-  double _width = 50;
-  double _height = 50;
-  Color _color = Colors.blue;
-  BorderRadiusGeometry _borderRadius = BorderRadius.circular(50);
-  bool _tap = true;
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              final random = Random();
-
-//              switch (_tap) {
-//                case false:
-//                  {
-//                    _width = 100;
-//                    _height = 100;
-//                    _color = Colors.red;
-//                    _borderRadius = BorderRadius.circular(100);
-//                  }
-//                  break;
-//                default:
-//                  {_width = 100;
-//                  _height = 100;
-//                  _color = Colors.red;
-//                  _borderRadius = BorderRadius.circular(100);}
-//              }
-
-              //change the width and the height randomly
-              _width = random.nextInt(300).toDouble();
-              _height = random.nextInt(300).toDouble();
-
-              //change the color randomly
-              _color = Color.fromRGBO(
-                random.nextInt(256),
-                random.nextInt(256),
-                random.nextInt(256),
-                1,
-              );
-
-              //change the border radius randomly.
-              _borderRadius =
-                  BorderRadius.circular(random.nextInt(100).toDouble());
-            });
-          },
-          child: Icon(Icons.play_arrow),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                AnimatedContainer(
-                  duration: Duration(seconds: 1),
-                  width: _width,
-                  height: _height,
-                  decoration:
-                      BoxDecoration(color: _color, borderRadius: _borderRadius),
-                  curve: Curves.fastOutSlowIn,
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[],
+          )
+        ],
+      ),
+    ));
   }
 }
